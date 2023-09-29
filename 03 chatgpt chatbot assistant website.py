5794409943ba393c7db00a61b276ba6002daa987
Prince Gupta <59447772+prince19998@users.noreply.github.com>
2023-09-29 11:37:13 +0530

    Add files via upload

---
+++ 03 chatgpt chatbot assistant website.py
@@ -0,0 +1,20 @@
+import openai
+import gradio
+
+openai.api_key = "Put your GPT API"
+
+messages = [{"role": "system", "content": "You Are AI Experts And I Appreciated You"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "I Appreciated You")
+
+demo.launch(share=True)
